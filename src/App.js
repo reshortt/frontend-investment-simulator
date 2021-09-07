4e2215f8c464770bd3cac7ef0edbb0b2d1b74e55
rshortt <rshortt@xilinx.com>
2021-09-07 09:12:37 -0700

    display some data from assets db on front-end - full stack connected

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import fetch from 'node-fetch'
 
 function App() {
+
+  const [result, setResult] = useState()
+  const [error, setError] = useState()
+
+  useEffect(()=> {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3005/investors/fetch_all")
+      const data = await response.json()
+      console.log(data)
+      setResult(data)
+    }
+    if (!result && !error) {
+      fetchData()
+    }
+
+  }, [result, error])
+
+
+  if (!result && !error) {
+    return <div>Loading</div>
+  }
+
+  if (error) {
+    return <div> Error : {error} </div>
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {result.map(item => <div>{item.symbol}: {item.name}</div>)}
       </header>
     </div>
   );
